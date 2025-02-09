1f19e091b9d7ecf6c47d74ad865986e7a1ecae80
Alex Mati <alexmati100@gmail.com>
2025-02-09 21:53:38 +0000

    feat: create organisation & preprod, prod ou

---
+++ aws-free-tier-config/main.tf
@@ -0,0 +1,16 @@
+# Create an AWS Organization
+resource "aws_organizations_organization" "org" {
+  feature_set = "ALL"
+}
+
+# Create Pre-production Organizational Unit
+resource "aws_organizations_organizational_unit" "preprod" {
+  name      = "preprod"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+# Create Production Organizational Unit
+resource "aws_organizations_organizational_unit" "prod" {
+  name      = "production"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
