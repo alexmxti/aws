1403a5628e5ed3343146af7e1c4a0fa6f8240961
Alex Mati <alexmati100@gmail.com>
2025-02-16 21:48:51 +0000

    feat: terraform aws auth

--- aws-account-management/main.tf
+++ aws-account-management/main.tf
@@ -11,6 +11,6 @@ resource "aws_organizations_organizational_unit" "preprod" {
 
 # Create Production Organizational Unit
 resource "aws_organizations_organizational_unit" "prod" {
-  name = "production"
+  name      = "production"
   parent_id = aws_organizations_organization.org.roots[0].id
 }
--- aws-account-management/provider.tf
+++ aws-account-management/provider.tf
@@ -8,5 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.AWS_REGION
+
+  assume_role {
+    role_arn     = var.AWS_ROLE_ARN
+    session_name = var.AWS_ROLE_SESSION_NAME
+  }
 }
