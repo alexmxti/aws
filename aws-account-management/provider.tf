65cdcfae03f6e5b423cd1a6c10572279f5f9bbfd
Alex Mati <alexmati100@gmail.com>
2025-02-18 20:12:38 +0000

    refactor: removes variables

--- aws-account-management/provider.tf
+++ aws-account-management/provider.tf
@@ -12,11 +12,5 @@ terraform {
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-  profile    = var.AWS_PROFILE
   region     = var.AWS_REGION
-
-  assume_role {
-    role_arn     = var.AWS_ROLE_ARN
-    session_name = var.AWS_ROLE_SESSION_NAME
-  }
 }
--- aws-account-management/variables.tf
+++ aws-account-management/variables.tf
@@ -10,26 +10,8 @@ variable "AWS_ACCESS_KEY_ID" {
   default     = ""
 }
 
-variable "AWS_PROFILE" {
-  description = "AWS Profile Name"
-  type        = string
-  default     = ""
-}
-
 variable "AWS_REGION" {
   description = "Default Region"
   type        = string
   default     = "eu-west-2"
-}
-
-variable "AWS_ROLE_ARN" {
-  description = "Role ARN for the Deployment User"
-  type        = string
-  default     = ""
-}
-
-variable "AWS_ROLE_SESSION_NAME" {
-  description = "Deployment User Role Session"
-  type        = string
-  default     = "DeploymentUserSession"
 }
