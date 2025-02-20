c0159c88aef1c0c668325d78986fe7e81a780d3f
Alex Mati <alexmati100@gmail.com>
2025-02-20 22:40:04 +0000

    refactor: remove vars

--- aws-account-management/variables.tf
+++ aws-account-management/variables.tf
@@ -10,18 +10,6 @@ variable "AWS_ACCESS_KEY_ID" {
   default     = ""
 }
 
-variable "AWS_ROLE_ARN" {
-  description = "Role ARN"
-  type        = string
-  default     = ""
-}
-
-variable "AWS_ROLE_SESSION_NAME" {
-  description = "Role Session Name"
-  type        = string
-  default     = ""
-}
-
 variable "AWS_REGION" {
   description = "Default Region"
   type        = string
