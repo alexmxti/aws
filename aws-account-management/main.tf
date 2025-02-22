11b38467c00b2c215a791e43cf067a1adf5fcdbd
Alex Mati <alexmati100@gmail.com>
2025-02-22 18:29:31 +0000

    ci: cleanup

--- aws-account-management/main.tf
+++ aws-account-management/main.tf
@@ -1,19 +1,19 @@
 # Create an AWS Organization
-resource "aws_organizations_organization" "org" {
-  feature_set = "ALL"
-}
+# resource "aws_organizations_organization" "org" {
+#   feature_set = "ALL"
+# }
 
-# Create Pre-production Organizational Unit
-resource "aws_organizations_organizational_unit" "preprod" {
-  name      = "preprod"
-  parent_id = aws_organizations_organization.org.roots[0].id
-}
+# # Create Pre-production Organizational Unit
+# resource "aws_organizations_organizational_unit" "preprod" {
+#   name      = "preprod"
+#   parent_id = aws_organizations_organization.org.roots[0].id
+# }
 
-# Create Production Organizational Unit
-resource "aws_organizations_organizational_unit" "prod" {
-  name      = "production"
-  parent_id = aws_organizations_organization.org.roots[0].id
-}
+# # Create Production Organizational Unit
+# resource "aws_organizations_organizational_unit" "prod" {
+#   name      = "production"
+#   parent_id = aws_organizations_organization.org.roots[0].id
+# }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
